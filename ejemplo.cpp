d0f62ddfc8e035d9fcfcaa8905e9ebdf3d73870d
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-18 23:02:19 -0500

    Actualiza ejemplo: simplificaciones por el uso repetido de for.

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -18,40 +18,37 @@ int main(){
         cout<<"Nombre: ";
         cin.ignore();
         getline(cin,EMPLEADOS[i].nom);
+        EMPLEADOS[i].ventot=0;
         cout<<"Ventas del ano:\n";
         for(int j=0; j<12; j++){
             cout<<"Mes "<<j+1<<": ";    cin>>EMPLEADOS[i].ven[j];
+            EMPLEADOS[i].ventot = EMPLEADOS[i].ventot + EMPLEADOS[i].ven[j];
         }
         cout<<"Salario: ";  cin>>EMPLEADOS[i].sal;
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<12; j++)
-            EMPLEADOS[i].ventot = EMPLEADOS[i].ventot + EMPLEADOS[i].ven[j];
+        if(EMPLEADOS[i].ventot > 100)
+            EMPLEADOS[i].sal = EMPLEADOS[i].sal*1.1;
     }
     maxven = EMPLEADOS[0].ventot;
     numemp = 0;
     for(int i=1; i<n; i++){
-        if(EMPLEADOS[i].ventot > maxven)
+        if(EMPLEADOS[i].ventot > maxven){
             maxven = EMPLEADOS[i].ventot;
             numemp = i;
+        }
     }
     cout<<"\nEMPLEADO CON MAYOR VENTA TOTAL\n";
     cout<<"Numero: "<<EMPLEADOS[numemp].num<<endl;
     cout<<"Nombre: "<<EMPLEADOS[numemp].nom<<endl;
-    for(int i=1; i<n; i++){
-        if(EMPLEADOS[i].ventot > 100)
-            EMPLEADOS[i].sal = EMPLEADOS[i].sal*1.1;
-    }
     cout<<"\nSALARIOS\n";
     for(int i=0; i<n; i++){
         cout<<"Empleado #"<<EMPLEADOS[i].num<<": "
             <<EMPLEADOS[i].sal<<endl;
     }
-    cout<<"\nVENTAS DE DICIEMBRE MENORES A 30\n";
+    cout<<"\nVENTAS DE DICIEMBRE MENORES A 30";
     for(int i=0; i<n; i++){
         if(EMPLEADOS[i].ven[11]<30){
             cout<<"\nNumero: "<<EMPLEADOS[i].num<<endl;
-            cout<<"Nombre: "<<EMPLEADOS[i].nom<<endl;
+            cout<<"Nombre: "<<EMPLEADOS[i].nom<<endl<<endl;
         }
     }
     return 0;
